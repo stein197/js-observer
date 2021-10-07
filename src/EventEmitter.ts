94272d5e8125f5a3d4e1109a7c565742c7ff7cef
Nail Gafarov <nil20122013@gmail.com>
2021-10-07 09:50:35 +0500

    +i(EventEmitter)

---
+++ src/EventEmitter.ts
@@ -0,0 +1,6 @@
+export interface EventEmitter<T extends {[K: string]: (...data: any[]) => void}> {
+
+	addEventListener<K extends keyof T>(key: K, listener: T): void;
+
+	removeEventListener<K extends keyof T>(key: K, listener: T): void;
+}
