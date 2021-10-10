0343bf8495e7ec8798857b04c05555c6d97f9a10
Nail <nil20122013@gmail.com>
2021-10-10 12:56:33 +0500

    Added documentation to EventEmitter

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,7 +1,20 @@
-// TODO: Documentation
+/**
+ * Interface that every class must implement if it emits events.
+ * @typeParam T - `<event name>: <event listener signature>` map that describes which events the class could emit.
+ */
 export interface EventEmitter<T extends {[K: string]: (...data: any[]) => void}> {
 
+	/**
+	 * Add a listener to specific event.
+	 * @param key Event name to subscribe on.
+	 * @param listener Listener.
+	 */
 	addEventListener<K extends keyof T>(key: K, listener: T): void;
 
+	/**
+	 * Remove previously subscribed listener from specific event.
+	 * @param key Event name to cancel subscribe.
+	 * @param listener Listener to remove.
+	 */
 	removeEventListener<K extends keyof T>(key: K, listener: T): void;
 }
