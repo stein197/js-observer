75e21c98ccce8e6ac6debed93ed6fe6520b199f6
Nail Gafarov <nil20122013@gmail.com>
2021-10-11 13:06:52 +0500

    +class(Value)

--- index.ts
+++ index.ts
@@ -99,6 +99,38 @@ export class EventDispatcher<T extends {[K: string]: (...data: any[]) => void}> 
 	}
 }
 
+export class Value<T> implements EventEmitter<ValueEvent<T>> {
+
+	private readonly dispatcher = new EventDispatcher<ValueEvent<T>>();
+
+	public get value(): Nullable<T> {
+		return this.__value;
+	}
+
+	public set value(value: Nullable<T>) {
+		if (this.__value === value)
+			return;
+		try {
+			this.validate?.(value);
+		} catch (e) {
+			this.dispatcher.notify("Error", e, value);
+			return;
+		}
+		this.__value = value;
+		this.dispatcher.notify("Change", value);
+	}
+
+	public constructor(private __value?: Nullable<T>, private readonly validate?: (value?: Nullable<T>) => void) {}
+
+	public addEventListener<K extends keyof ValueEvent<T>>(key: K, listener: ValueEvent<T>[K]): void {
+		this.dispatcher.addEventListener(key, listener);
+	}
+
+	public removeEventListener<K extends keyof ValueEvent<T>>(key: K, listener: ValueEvent<T>[K]): void {
+		this.dispatcher.removeEventListener(key, listener);
+	}
+}
+
 /**
  * Interface that every class must implement if it emits events.
  * @typeParam T - `<event name>: <event listener signature>` map that describes which events the class could emit.
@@ -119,3 +151,10 @@ export interface EventEmitter<T extends {[K: string]: (...data: any[]) => void}>
 	 */
 	removeEventListener<K extends keyof T>(key: K, listener: T[K]): void;
 }
+
+type ValueEvent<T> = {
+	Change(value?: Nullable<T>): void;
+	Error(error: any, value?: Nullable<T>): void;
+}
+
+type Nullable<T> = T | null | undefined;
