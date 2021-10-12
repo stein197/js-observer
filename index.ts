5dcc0d716130207f0b323216d3e803d39a51a56e
Nail Gafarov <nil20122013@gmail.com>
2021-10-12 15:56:50 +0500

    Added Observable export

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export {Observer} from "./src/Observer";
+export {Observable} from "./src/Observable";
 export {EventDispatcher} from "./src/EventDispatcher";
-export {Value} from "./src/Value";
 export {EventEmitter} from "./src/EventEmitter";
