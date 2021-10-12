5d61fe21e3bcd530d062c9b46a37438f8f22951b
Nail Gafarov <nil20122013@gmail.com>
2021-10-12 15:51:19 +0500

    Clean scripts now deletes directories

--- clean.bat
+++ clean.bat
@@ -1,3 +1,3 @@
 @echo off
-del *.js >nul 2>&1
-del *.d.ts >nul 2>&1
+rmdir /s /q out >nul 2>&1
+rmdir /s /q types >nul 2>&1
--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
-rm *.js 2> /dev/null
-rm *.d.ts 2> /dev/null
+rm -rf out 2> /dev/null
+rm -rf types 2> /dev/null
